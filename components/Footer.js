abf4812bbdcd62e3830654e09fed18b582b8028a
Aditya Prabhat <adityaprabhat1998@gmail.com>
2021-04-25 12:30:21 +0530

    init commit

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div>
+      <p>Cpoyright 2021 Ninja List</p>
+    </div>
+  )
+}
+
+export default Footer
---
+++ pages/404.js
@@ -0,0 +1,25 @@
+import Link from 'next/link'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
+const NotFound = () => {
+
+  const router = useRouter()
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push('./')
+    }, 3000)
+  }, [])
+
+  return (
+    <div>
+      Not Found
+      <h1>Oooops...</h1>
+      <h2>Page cannot be found</h2>
+      <p>Go back to <Link href="/"><a>Homepage</a></Link> </p>
+    </div>
+  )
+}
+
+export default NotFound
